2ae6b1eacc5729c214fa5fc123c9d2a8fdc81fad
OsBoss16 <128861990+OsBoss16@users.noreply.github.com>
2023-03-29 17:40:28 +0100

    Add files via upload

--- animal picker.py
+++ animal picker.py
@@ -1,12 +1,15 @@
 import random
 animals = ["dog", "cat", "horse"]
-listLength = len(animals)
+listLength = (len(animals)) - 1
 
 animalChoice = random.randint(0, listLength)
 theAnimal = animals[animalChoice]
 print(theAnimal, animalChoice)
 
-class Dog:
-    def __init__(self, name, age) -> None:
-        
-        
+dog = {
+    "vertebrate": True
+}
+
+x = input()
+if x == "vertebrate" or "Vertebrate" or "VERTEBRATE":
+    print(dog["vertebrate"])
